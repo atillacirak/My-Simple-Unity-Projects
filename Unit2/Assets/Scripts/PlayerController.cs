225df5916b44750f3c77d94b73f4de8625d98e37
Atilla Çırak <ati_cirak@outlook.com>
2023-11-26 23:31:40 +0300

    Karakter eklendi, hareket ve sınır scriptleri yazıldı.

--- Unit2/Assets/Scripts/PlayerController.cs
+++ Unit2/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 10.0f;
+    private float horizontal_input;
+    private float boundry = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +16,15 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+        horizontal_input = Input.GetAxis("Horizontal"); 
+        transform.Translate(Vector3.right * speed * Time.deltaTime * horizontal_input);
+
+        if (transform.position.x < -boundry){
+            transform.position = new Vector3(-boundry, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > boundry){
+            transform.position = new Vector3(boundry, transform.position.y, transform.position.z);
+        }
     }
 }
