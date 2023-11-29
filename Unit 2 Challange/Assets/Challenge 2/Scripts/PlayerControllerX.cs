e04d97fc27b101f9cae7ef4c6692caf4ee98ded1
Atilla Çırak <ati_cirak@outlook.com>
2023-11-29 21:33:02 +0300

    Kodda güncellemeler

--- Unit 2 Challange/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Unit 2 Challange/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float timeRequired = 2.0f;
+    private float timeNow;
 
     // Update is called once per frame
     void Update()
     {
+        timeNow -= Time.deltaTime;
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (timeNow <= 0 && Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timeNow = timeRequired;
         }
     }
 }
